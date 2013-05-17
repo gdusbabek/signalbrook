1460e9f830154c9db6db2c4d51aa5de945938be5
gdusbabek <gdusbabek@gmail.com>
2013-05-17 15:01:16 -0500

    LossyCounting algorithm.

---
+++ src/main/java/signalbrook/core/LossyCounting.java
@@ -0,0 +1,77 @@
+package signalbrook.core;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class LossyCounting<T> implements Stream<T> {
+    private boolean over = false;
+    private long housekeepingCount = 0;
+    
+    private long n = 0;
+    private double delta = 0;
+    private final Map<T, Tuple> stored = new HashMap<T, Tuple>();
+    
+    private final double k;
+    
+    public LossyCounting(double error) {
+        this.k = 1d / error;
+    }
+    
+    public void observe(T item) throws Exception {
+        if (n == Long.MAX_VALUE) {
+            over = true;
+        } else {
+            n += 1;
+        }
+        
+        Tuple t = stored.get(item);
+        if (t != null) {
+            t.lowerBound += 1;
+        } else {
+            t = new Tuple();
+            t.item = item;
+            t.lowerBound = 1;
+            t.delta = Math.floor((double)(n-1) / k);
+            stored.put(item, t);
+        }
+        
+        // housekeeping. different heuristics can work here.
+//        if ((double)n / k != this.delta) {
+        if (n % 1000 == 0) {
+            housekeepingCount += 1;
+            delta = (double)n / k;
+            List<T> toRemove = new ArrayList<T>();
+            for (Map.Entry<T, Tuple> entry : stored.entrySet()) {
+                if (entry.getValue().lowerBound < delta) {
+                    toRemove.add(entry.getKey());
+                }
+            }
+            for (T key : toRemove) {
+                stored.remove(key);
+            }
+        }
+    }
+    
+    public List<Count> getFrequentItems() {
+        List<Count> list = new ArrayList<Count>();
+        for (Map.Entry<T, Tuple> entry : stored.entrySet()) {
+            list.add(new Count(entry.getKey(), entry.getValue().lowerBound));
+        }
+        Collections.sort(list);
+        return list;
+    }
+
+    public boolean hasOverflowed() {
+        return over;
+    }
+    
+    private class Tuple {
+        private T item;
+        private long lowerBound;
+        private double delta;
+    }
+}
--- src/test/java/signalbrook/core/FrequentTest.java
+++ src/test/java/signalbrook/core/FrequentTest.java
@@ -11,15 +11,15 @@ import java.util.List;
 public class FrequentTest {
     
     @Test
-    public void simpleTest() throws Exception {
-        Frequent<Long> freq = new Frequent<Long>(0.001d);
+    public void frequentTest() throws Exception {
+        Frequent<Long> stream = new Frequent<Long>(0.001d);
         
         for (int i = 0; i < 1000000; i++) {
-            freq.observe((long)Random.discrete(Random.DISCRETE_BIMODAL_500));
+            stream.observe((long) Random.discrete(Random.DISCRETE_BIMODAL_500));
         }
         
         // ok. lets
-        List<Count> frequentItems = freq.getFrequentItems();
+        List<Count> frequentItems = stream.getFrequentItems();
         Collections.reverse(frequentItems);
         
         // these should represent the two modes of the bimodal distribution (50 and 450). counts should be roughly equal.
@@ -33,4 +33,29 @@ public class FrequentTest {
         Assert.assertEquals(50L, topTwo.get(0).getItem());
         Assert.assertEquals(450L, topTwo.get(1).getItem());
     }
+    
+    @Test
+    public void lossyTest() throws Exception {
+        LossyCounting<Long> stream = new LossyCounting<Long>(0.001d);
+        for (int i = 0; i < 1000000; i++) {
+            stream.observe((long) Random.discrete(Random.DISCRETE_BIMODAL_500));
+        }
+        
+        // ok. lets
+        List<Count> frequentItems = stream.getFrequentItems();
+        Collections.reverse(frequentItems);
+        
+        // these should represent the two modes of the bimodal distribution (50 and 450). counts should be roughly equal.
+        List<Count> topTwo = Lists.newArrayList(frequentItems.get(0), frequentItems.get(1));
+        Collections.sort(topTwo, new Comparator<Count>() {
+            public int compare(Count o1, Count o2) {
+                return (int)((Long)o1.getItem() - (Long)o2.getItem());
+            }
+        });
+        
+        Assert.assertEquals(50L, topTwo.get(0).getItem());
+        Assert.assertEquals(450L, topTwo.get(1).getItem());
+    }
+    
+    
 }
